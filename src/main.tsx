594109026f62a9de881a2104cf5701855177a68f
Elad Ratson <elad.ratson@vayehee.com>
2026-07-07 19:02:58 +0200

    Use precise favicon spacing

--- src/main.tsx
+++ src/main.tsx
@@ -146,7 +146,6 @@ function App() {
           <div className="brand-byline-layer" aria-hidden="true">
             <div className="brand-byline">
               <span className="brand-byline-by">by</span>
-              {"  "}
               <img
                 className="brand-byline-favicon"
                 src="https://vayehee.com/wp-content/uploads/2021/11/cropped-cropped-logo-small-1-32x32.png"
